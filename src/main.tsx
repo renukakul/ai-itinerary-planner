47b371e2dc8e5e4c91987a1f1105c84d62bbeeff
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2025-04-15 13:32:05 -0400

    resolved initial setup isuues and configured Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,26 @@
+import * as React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.js'
-import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import CreateTrip from './create-trip/CreateTrip'
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
-} else {
-  console.error("Root element not found");
-}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/create-trip',
+    element: <CreateTrip />,
+
+  }
+])
+
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
