6030f4520dc1c669631bfae36567968159c38bf6
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2025-04-15 09:44:59 -0400

    Tailwind and ts setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from 'react'
+import { Button } from './components/ui/button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Button>Plan Your Trip</Button>
+    </>
+  )
+}
+
+export default App
