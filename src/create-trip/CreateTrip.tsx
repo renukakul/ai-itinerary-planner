0f048ad2d62ccecffebbf2dd1eaccc993e35933e
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2025-04-15 20:37:22 -0400

    integrate Google Maps APIs for autocomplete and location search

--- src/create-trip/CreateTrip.tsx
+++ src/create-trip/CreateTrip.tsx
@@ -1,6 +1,7 @@
-import * as  React from 'react'
+import * as React from 'react';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
-function CreateTrip() {
+const CreateTrip: React.FC = () => {
   return (
     <div className='sm:px md:px lg:px-56 xl:px-96 2xl:px-96 px-5 mt-10'>
       <h2 className='font-bold text-3xl'>Tell us your travel preference</h2>
@@ -8,6 +9,7 @@ function CreateTrip() {
         We will use this information to create a personalized trip for you.
       </p>
       <div className='flex flex-col gap-5 mt-10'>
+        {/* Trip Type */}
         <div className='flex flex-col gap-2'>
           <label htmlFor='trip-type' className='text-gray-500 text-[20px]'>Trip Type</label>
           <select id='trip-type' className='border border-gray-300 rounded-md p-2'>
@@ -16,23 +18,45 @@ function CreateTrip() {
             <option value='leisure'>Leisure</option>
             <option value='adventure'>Adventure</option>
           </select>
-          </div>
+        </div>
+
+        {/* Trip Destination */}
         <div className='flex flex-col gap-2'>
           <label htmlFor='trip-destination' className='text-gray-500 text-[20px]'>Trip Destination</label>
-          <input type='text' id='trip-destination' className='border border-gray-300 rounded-md p-2' placeholder='Enter your destination' />
-          </div>
+            <GooglePlacesAutocomplete 
+            apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY} // Type assertion for the API key
+            />
+          
+        </div>
+
+        {/* Trip Dates */}
         <div className='flex flex-col gap-2'>
           <label htmlFor='trip-dates' className='text-gray-500 text-[20px]'>Trip Dates</label>
-          <input type='date' id='trip-start-date' className='border border-gray-300 rounded-md p-2' />
-          <input type='date' id='trip-end-date' className='border border-gray-300 rounded-md p-2' />
+          <input 
+            type='date' 
+            id='trip-start-date' 
+            className='border border-gray-300 rounded-md p-2' 
+          />
+          <input 
+            type='date' 
+            id='trip-end-date' 
+            className='border border-gray-300 rounded-md p-2' 
+          />
         </div>
+
+        {/* Trip Budget */}
         <div className='flex flex-col gap-2'>
           <label htmlFor='trip-budget' className='text-gray-500 text-[20px]'>Trip Budget</label>
-          <input type='number' id='trip-budget' className='border border-gray-300 rounded-md p-2' placeholder='Enter your budget' />
+          <input 
+            type='number' 
+            id='trip-budget' 
+            className='border border-gray-300 rounded-md p-2' 
+            placeholder='Enter your budget' 
+          />
         </div>
-          </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default CreateTrip;
