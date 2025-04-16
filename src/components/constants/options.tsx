86426dc2b3f883a83ae54213a67d129f22713c7e
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2025-04-15 22:10:48 -0400

    added constants for trip budget and headcount

---
+++ src/components/constants/options.tsx
@@ -0,0 +1,56 @@
+
+import { Plane, Heart, Users, icons } from "lucide-react";
+export const SelectTravelesList = [
+    {
+        id: 1,
+        name: "Just Me",
+        desc: "A solo traveler seeking adventure and exploration.",
+        icon: '🧍‍♂️✨',
+        people: '1'
+    },
+    {
+        id: 2,
+        name: "A Couple",
+        desc: "A couple looking for a romantic getaway.",
+        icon: '🥂',
+        people: '2'
+    },
+    {
+        id: 3,
+        name: "A Family",
+        desc : "A group of friends or family traveling together.",
+        icon: '👨‍👩‍👧‍👦',
+        people: '3-10'
+
+    },
+    {
+        id: 4,
+        name: "Friends",
+        desc: "A group of friends looking for a fun trip.",
+        icon: '👫👬👯‍♂️',
+        people: '10-50'
+    },
+   
+]
+
+export const TripBudgetList = [
+    {
+        id: 1,
+        name: "Cheap",
+        desc: "A budget-friendly trip with minimal expenses.",
+        icons: '💵',
+    },
+    {
+        id: 2,
+        name: "Medium",
+        desc: "A moderate budget for a comfortable trip.",
+        icons: '💰',
+
+    },
+    {
+        id: 3,
+        name: "Luxury", 
+        desc: "A luxurious trip with premium experiences.",
+        icons: '👑',
+    }
+]
--- src/create-trip/CreateTrip.tsx
+++ src/create-trip/CreateTrip.tsx
@@ -1,8 +1,11 @@
 import { on } from 'events';
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { SingleValue } from 'react-select';
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import { Input } from '@/components/ui/input';
+import { SelectTravelesList, TripBudgetList } from '@/components/constants/options';
+import { Button } from '@/components/ui/button';
 
 const CreateTrip: React.FC = () => {
   // Define the type for the place option
@@ -13,32 +16,39 @@ const CreateTrip: React.FC = () => {
   };
   
   const [place, setPlace] = useState<SingleValue<PlaceOption>>(null);
+
+  const [formdata, setFormData] = useState<Record<string, any>>({});
+  
+  const handleInputChange = (name: string, value: any) => {
+    setFormData({
+      ...formdata,
+      [name]: value,
+    });
+    console.log('formdata', formdata);
+  }
+  useEffect(() => {
+    console.log('formdata', formdata);
+  }, [formdata]);
+  // This effect will run whenever formdata changes
+  
+  
   return (
     <div className='sm:px md:px lg:px-56 xl:px-96 2xl:px-96 px-5 mt-10'>
       <h2 className='font-bold text-3xl'>Tell us your travel preference</h2>
       <p className='text-gray-500 text-[20px]'>
         We will use this information to create a personalized trip for you.
       </p>
-      <div className='flex flex-col gap-5 mt-10'>
-        {/* Trip Type */}
-        <div className='flex flex-col gap-2'>
-          <label htmlFor='trip-type' className='text-gray-500 text-[20px]'>Trip Type</label>
-          <select id='trip-type' className='border border-gray-300 rounded-md p-2'>
-            <option value=''>Select Trip Type</option>
-            <option value='business'>Business</option>
-            <option value='leisure'>Leisure</option>
-            <option value='adventure'>Adventure</option>
-          </select>
-        </div>
-
-        {/* Trip Destination */}
+      {/* Trip Destination */}
         <div className='flex flex-col gap-2'>
           <label htmlFor='trip-destination' className='text-gray-500 text-[20px]'>Trip Destination</label>
             <GooglePlacesAutocomplete 
             apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY} // Type assertion for the API key
             selectProps={{
               value: place,
-              onChange: (v) => { setPlace(v); console.log(v); },
+              onChange: (v) => { setPlace(v); console.log(v); 
+                handleInputChange('location',v);
+                console.log(formdata);
+                console.log('location',v);},
             }}
             />
           
@@ -47,12 +57,12 @@ const CreateTrip: React.FC = () => {
         {/* Trip Dates */}
         <div className='flex flex-col gap-2'>
           <label htmlFor='trip-dates' className='text-gray-500 text-[20px]'>Trip Dates</label>
-          <input 
+          <Input
             type='date' 
             id='trip-start-date' 
             className='border border-gray-300 rounded-md p-2' 
           />
-          <input 
+          <Input
             type='date' 
             id='trip-end-date' 
             className='border border-gray-300 rounded-md p-2' 
@@ -60,17 +70,41 @@ const CreateTrip: React.FC = () => {
         </div>
 
         {/* Trip Budget */}
-        <div className='flex flex-col gap-2'>
+        <div>
+        <div className='text-xl my-3 font-medium'>
           <label htmlFor='trip-budget' className='text-gray-500 text-[20px]'>Trip Budget</label>
-          <input 
-            type='number' 
-            id='trip-budget' 
-            className='border border-gray-300 rounded-md p-2' 
-            placeholder='Enter your budget' 
-          />
+          <div className='grid grid-cols-3 gap-5 mt-5'>
+            {TripBudgetList.map(((item, index) => (
+              <div key={index} className='p-4 border rounded-xl  hover:shadow-lg cursor-pointer '>
+                <h2 className='text-3xl'>{item.icons}</h2>
+                <h2 className='font-bold text-xl'>{item.name}</h2>
+                <p className='text-gray-500'>{item.desc}</p>
+          </div>
+            )))}
         </div>
       </div>
+      </div>
+      <div>
+      <div className='text-xl my-3 font-medium'>
+          <label htmlFor='trip-budget' className='text-gray-500 text-[20px]'>Head count</label>
+          <div className='grid grid-cols-3 gap-5 mt-5'>
+            {SelectTravelesList.map(((item, index) => (
+              <div key={index} className='p-4 border rounded-xl  hover:shadow-lg cursor-pointer '>
+                <h2 className='text-3xl'>{item.icon}</h2>
+                <h2 className='font-bold text-xl'>{item.name}</h2>
+                <p className='text-gray-500'>{item.desc}</p>
+          </div>
+            )))}
     </div>
+    </div>
+    </div>
+    
+
+    <div className='my-10 justify-end flex'>
+      <Button>My Trip Plan</Button>
+    </div>
+    </div>
+
   );
 };
 
